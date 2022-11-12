01564647bf3f98d16a7ac6cdd9eea4743adfe98b
MashinaSSokom <skltan@mail.ru>
2022-11-12 16:19:09 +0300

    remove force update router

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,9 @@ function App() {
     return (
         <Layout>
             <Navbar/>
-            <AppRouter/>
+            <Layout.Content>
+                <AppRouter/>
+            </Layout.Content>
         </Layout>
     );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import {Layout, Menu, Row} from "antd";
 import {RouteNames} from "../router";
-import {Redirect, Route, useHistory} from "react-router-dom";
+import {useHistory} from "react-router-dom";
 
 const Navbar = () => {
-    const history = useHistory()
-    // history.push('/story/123')
+    let history = useHistory()
+
     return (
         <Layout.Header>
             <Row justify={'start'}>
                 <Menu theme={"dark"} mode={"horizontal"} selectable={false}>
-                    <Menu.Item onClick={() => history.push(RouteNames.HOME)}
-                               key={1}>
+                    <Menu.Item onClick={() => history.push(RouteNames.HOME)} key={1}>
                         Главная
                     </Menu.Item>
                 </Menu>
