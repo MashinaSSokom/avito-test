2e63386a8bbfb929da03173bbea5788dcea7dcdb
MashinaSSokom <skltan@mail.ru>
2022-11-13 19:22:16 +0300

    add comment model

---
+++ src/models/IComment.ts
@@ -0,0 +1,13 @@
+export interface IComment {
+    by: string;
+    id: number;
+    kids?: [number];
+    parent: number;
+    text: string;
+    time: number;
+    type: string;
+}
+
+export interface ICommentIdList {
+    id: [number]
+}
